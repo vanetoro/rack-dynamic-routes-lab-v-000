c001cfc5cea558f28b2d56a6ea4b722f926ad37f
Vanessa Toro <vanetoro84@gmail.com>
2018-05-04 16:49:20 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,13 +8,13 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-  req.path.match(/items/)
+if  req.path.match(/items/)
       item_search = req.params['items']
-      # binding.pry
+  else
   #    resp.write "#{item.price}"
   #  else
-  #    resp.write "Route not found"
-  #    resp.status = 404
-
+     resp.write "Route not found"
+     resp.status = 404
+end
   end
 end
