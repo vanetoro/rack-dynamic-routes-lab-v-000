ebfd265319cfadd6a49138934a4437914084edbf
Vanessa Toro <vanetoro84@gmail.com>
2018-05-04 17:08:42 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -13,11 +13,12 @@ class Application
 
       if  item = @@items.find {|i| i.name == item_name}
             resp.write "#{item.price}"
+        end
         else
          resp.write "Route not found"
          resp.status = 404
      end
    resp.finish
   end
-end
+
 end
