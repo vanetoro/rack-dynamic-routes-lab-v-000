8f0593cdcec2dd726b9f2fe8717594ccf1a49092
Vanessa Toro <vanetoro84@gmail.com>
2018-05-04 17:10:56 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,25 +1,9 @@
-
-require'pry'
-
 class Application
-  def call(env)
-    @@items =[]
+def call
+ resp = Rack::Response.new 
+ req = Rack::Request.new(env)
 
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
-
-  if  req.path.match(/items/)
-    item_name = req.path.split("/items/").last
-
-      if  item = @@items.find {|i| i.name == item_name}
-        binding.pry
-            resp.write "#{item.price}"
-        end
-        else
-         resp.write "Route not found"
-         resp.status = 404
-     end
-   resp.finish
-  end
-
+end
+ 
+  
 end
