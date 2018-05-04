fc96bca1a209ad2561919e00a24bde3a5b8d8c6b
Vanessa Toro <vanetoro84@gmail.com>
2018-05-04 17:12:16 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,10 +3,11 @@ def call
  resp = Rack::Response.new
  req = Rack::Request.new(env)
 
-end
 
-if req.path(/items/)
-  resp.write 'Bad Route'
- req.status = 404
- end
+
+  if req.path(/items/)
+    resp.write 'Bad Route'
+   req.status = 404
+   end
+end
 end
