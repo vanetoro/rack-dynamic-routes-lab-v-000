33419610a0e2ff569a953589d2051e2674b58534
Vanessa Toro <vanetoro84@gmail.com>
2018-05-04 17:07:58 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,15 +8,16 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-      if  req.path.match(/items/)
-        item_name = req.path.split("/items/").last
-        item = @@items.find {|i| i.name == item_name}
-        binding.pry
-        resp.write "#{item.price}"
-    else
-     resp.write "Route not found"
-     resp.status = 404
-   end
+  if  req.path.match(/items/)
+    item_name = req.path.split("/items/").last
+
+      if  item = @@items.find {|i| i.name == item_name}
+            resp.write "#{item.price}"
+        else
+         resp.write "Route not found"
+         resp.status = 404
+     end
    resp.finish
   end
+end
 end
