ff2d1290f8cad05fbba1bb872a524a68447459bb
Vanessa Toro <vanetoro84@gmail.com>
2018-05-04 17:04:09 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
 
     if  req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      binding.pry
+
     item = @@items.find {|i| i.name == item_name}
         binding.pry
         resp.write "#{item.price}"
